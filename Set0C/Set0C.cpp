9d6f2e79eb01dffb4ea111ecd9e4660eeca73480
AstaikinEvgenii <astaikinevgenii@gmail.com>
2024-11-09 11:25:20 +0300

    09112024 1125
    Astaikin Evgenii

---
+++ Set0C/Set0C.cpp
@@ -0,0 +1,20 @@
+/*
+Set 0C
+student - Astaikin Evgenii
+09112024
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for(int a = 0; a <= 4; a += 1)
+    {
+        for (int j = 1; j <= 5; j += 1)
+    {
+        cout << "*";
+    }
+    cout << endl;
+}
+return 0;
+}
