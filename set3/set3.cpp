d471eb73c406526d662c5bf62adc140c68a43e38
AstaikinEvgenii <astaikinevgenii@gmail.com>
2024-11-09 11:14:08 +0300

    09112024 1113
    AstaikinEvgenii

---
+++ set3/set3.cpp
@@ -0,0 +1,17 @@
+/*
+Set 3
+student - AstaikinEvgenii
+09112024
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a{};
+    cout << "Please Enter the Celsius" << endl;
+    cin >> a;
+    double result = 1.8 * a + 32;
+    cout << "Celsius value: " << a << endl;
+    cout << "Fahrenheit:" << result << endl;
+    return 0;
+}
