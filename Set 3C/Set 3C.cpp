84088bfad0379cf16b4849b930cb5d77aa3ba3a0
AstaikinEvgenii <astaikinevgenii@gmail.com>
2024-11-15 16:41:28 +0300

    15112024
    Astaikin Evgenii

---
+++ Set 3C/Set 3C.cpp
@@ -0,0 +1,15 @@
+/*
+Set 1C
+student - Astaikin Evgenii
+15112024
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int n = 4;
+    int numbers[n] {11, 12, 13, 14};
+    for (int i = 0; i < n; i++) {
+        std::cout << numbers[i] << std::endl;
+    }
+}
