fd1755fd22c54bac93625d2f1cafe8b0be7d2112
AstaikinEvgenii <astaikinevgenii@gmail.com>
2024-11-16 10:39:50 +0300

    16112024
    Astaikin Evgenii

---
+++ Tricks/Tricks.cpp
@@ -0,0 +1,17 @@
+/*
+Tricks
+student - Astaikin Evgenii
+16112024
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x = 45;
+    int y = 54;
+
+    int z = (x > y) ? x : y;
+    cout << z << endl;
+
+    return 0;
+}
