79ce0fecb52c4d41b117b9f1acc172b76d78dc75
AstaikinEvgenii <astaikinevgenii@gmail.com>
2024-12-07 11:44:19 +0300

    07122024
    astaikin evgenii

---
+++ struct3/struct3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+struct worker
+{
+    string name;
+    int age;
+    short email;
+    int phone_number;
+    string education;
+};
+
+worker Litvin = ("Mixa" , 22 , "mixalitvinov@gmail.com" , +86054632144 , "МОУ СОШ");
+
+void printworker(worker*p)
+{
+    cout << p-> name << p ->phone_number << p-> age << p ->education p-> email << endl;
+}
+int main()
+{
+    printworker(&Litvin);
+    return 0;
+}
